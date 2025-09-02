d23f5a02c0518e1aaa9030f62630c41f4cd0b38b
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-02 19:31:04 +0300

    Fix typo in welcome message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Willkommen beim Version Kontrolle Tool!');
+  res.send('Willkommen beim Versionskontroll-Tool!');
 });
 
 app.listen(port, () => {
